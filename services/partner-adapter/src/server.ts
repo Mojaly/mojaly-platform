55b698a98edb5109a955f3cdef6260091dbc25e0
alphaxardjr <masterprogrammerg@gmail.com>
2026-07-18 11:07:06 +0300

    add partner adapter setup

---
+++ services/partner-adapter/src/server.ts
@@ -0,0 +1,16 @@
+import { buildApp } from './app.js'
+
+const app = buildApp()
+
+const start = async () => {
+  try {
+    await app.listen({
+      port: 4005,
+    })
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
